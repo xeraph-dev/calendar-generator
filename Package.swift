eeaf05bad082b01a17a89c5631797acd36071ac5
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-01-15 22:54:52 -0500

    implement api

--- Package.swift
+++ Package.swift
@@ -7,30 +7,34 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.macOS(.v12)],
     products: [
-        .executable(name: "cg", targets: ["CLI"]),
         .executable(name: "api", targets: ["API"]),
+        .executable(name: "cg", targets: ["CLI"]),
         .library(name: "CGCore", targets: ["CGCore"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-markdown.git", from: "0.3.0"),
         .package(url: "https://github.com/johnsundell/plot.git", from: "0.14.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+//        .package(name: "Vercel", path: "../swift-cloud/Vercel"),
         .package(url: "https://github.com/swift-cloud/Vercel.git", from: "2.1.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.91.1"),
     ],
     targets: [
+        .executableTarget(
+            name: "API",
+            dependencies: [
+                "CGCore",
+                .product(name: "Vercel", package: "Vercel"),
+                .product(name: "VercelVapor", package: "Vercel"),
+                .product(name: "Plot", package: "plot"),
+                .product(name: "Vapor", package: "vapor"),
+            ]
+        ),
         .executableTarget(
             name: "CLI",
             dependencies: [
                 "CGCore",
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-        .executableTarget(
-            name: "API",
-            dependencies: [
-                .product(name: "Vercel", package: "Vercel"),
-                .product(name: "VercelVapor", package: "Vercel"),
-                .product(name: "Plot", package: "plot"),
             ]
         ),
         .target(
--- Sources/API/entrypoint.swift
+++ Sources/API/entrypoint.swift
@@ -1,11 +1,143 @@
+import CGCore
+import Foundation
 import Vapor
 import VercelVapor
+
+struct Args: Content {
+    var type: String?
+    var locale: String?
+    var timeZone: String?
+    var year: String?
+    var month: String?
+    var firstWeekday: String?
+}
 
 @main
 struct API: VaporHandler {
     static func configure(app: Application) async throws {
-        app.get { _ in
-            "Hello, Vapor"
+        app.get { req throws -> Vapor.Response in
+            var calendar: Calendar = .init(identifier: .gregorian)
+            let args = try req.query.decode(Args.self)
+
+            if let locale = args.locale?.split(separator: "?").first {
+                calendar.locale = .init(identifier: .init(locale))
+            } else {
+                calendar.locale = .current
+            }
+
+            if let timeZone = args.timeZone?.split(separator: "?").first,
+               let timeZone: TimeZone = .init(identifier: .init(timeZone))
+            {
+                calendar.timeZone = timeZone
+            } else {
+                calendar.timeZone = .current
+            }
+
+            if let firstWeekDay = args.firstWeekday?.split(separator: "?").first {
+                if let firstWeekDay = Int(firstWeekDay) {
+                    guard firstWeekDay >= 1, firstWeekDay <= 7 else {
+                        throw Abort(.badRequest, reason: "firstWeekday must be an integer between 1 and 7")
+                    }
+                    calendar.firstWeekday = firstWeekDay
+                } else {
+                    let firstWeekday = firstWeekDay.lowercased()
+                    let index = [calendar.weekdaySymbols, calendar.shortWeekdaySymbols, calendar.veryShortWeekdaySymbols]
+                        .map { $0.map { $0.lowercased() }}
+                        .compactMap { $0.firstIndex(where: { $0.starts(with: firstWeekday) }) }
+                        .first
+                    guard let index = index else {
+                        throw Abort(.badRequest, reason: "firstWeekday must be a valid weekday")
+                    }
+                    calendar.firstWeekday = index + 1
+                }
+            }
+
+            let calendarMonth: Calendar.Month
+            if let month = args.month?.split(separator: "?").first {
+                if let month = Int(month) {
+                    guard month >= 1, month <= 12 else {
+                        throw Abort(.badRequest, reason: "month must be an integer between 1 and 12")
+                    }
+                    calendarMonth = .init(rawValue: month)!
+                } else {
+                    let month = month.lowercased()
+                    let index = [calendar.monthSymbols, calendar.shortMonthSymbols, calendar.veryShortMonthSymbols]
+                        .map { $0.map { $0.lowercased() }}
+                        .compactMap { $0.firstIndex(where: { $0.starts(with: month) }) }
+                        .first
+                    guard let index = index else {
+                        throw Abort(.badRequest, reason: "month must be a valid month")
+                    }
+                    calendarMonth = .init(rawValue: index + 1)!
+                }
+            } else {
+                calendarMonth = .init(rawValue: calendar.component(.month, from: Date()))!
+            }
+
+            let year = if let year = args.year?.split(separator: "?").first, let year: Int = .init(year) {
+                year
+            } else {
+                calendar.component(.year, from: Date())
+            }
+
+            let raw = calendarMonth.raw(calendar: calendar, year: year)
+
+            var calendarResult: RawCalendarResult = .raw
+
+            if let agent = req.headers.first(name: "User-Agent") {
+                if agent.contains("curl") || agent.contains("libcurl") {
+                    calendarResult = .raw
+                } else if agent.contains("Chrome") || agent.contains("Safari") || agent.contains("Firefox") || agent.contains("Mozilla") {
+                    calendarResult = .html
+                } else {
+                    calendarResult = .raw
+                }
+            }
+
+            if let accept = req.headers.accept.compactMap({ pref -> HTTPMediaType? in
+                if pref.mediaType == .any {
+                    nil
+                } else if pref.mediaType.description == "text/markdown" {
+                    pref.mediaType
+                } else {
+                    switch pref.mediaType {
+                    case .html, .json, .plainText: pref.mediaType
+                    default: nil
+                    }
+                }
+            }).first {
+                calendarResult = if accept.description == "text/markdown" {
+                    .markdown
+                } else {
+                    switch accept {
+                    case .html: .html
+                    case .json: .json
+                    case .plainText: .raw
+                    default: throw Abort(.unsupportedMediaType)
+                    }
+                }
+            }
+
+            if let type = args.type?.split(separator: "?").first, let type: RawCalendarResult = .init(rawValue: .init(type)) {
+                calendarResult = type
+            }
+
+            var headers = HTTPHeaders()
+            switch calendarResult {
+            case .html: headers.add(name: .contentType, value: "text/html")
+            case .markdown: headers.add(name: .contentType, value: "text/markdown")
+            case .json: headers.add(name: .contentType, value: "application/json")
+            case .raw: headers.add(name: .contentType, value: "text/plain")
+            }
+
+            let body = switch calendarResult {
+            case .raw: raw.description
+            case .json: String(data: raw.json(), encoding: .utf8)!
+            case .markdown: raw.markdown().format()
+            case .html: raw.html().render()
+            }
+
+            return .init(status: .ok, headers: headers, body: .init(string: body))
         }
     }
 }
